3e7a9e5980ca050672b07bab11dffe0eb40ae259
ahhxfeng <ahhxfeng@gmail.com>
2019-04-19 00:37:17 +0800

    '提交'

---
+++ flask_test/hello.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env pytthon
+#coding=utf-8
+
+__author__ = 'ahhxfeng'
+__version__ = '1.0.0'
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.routue('/')
+def index():
+    return '<h1>Hello world !!!</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
