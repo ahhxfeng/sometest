a3811f9121899a2be652ab78433e45251dab6f49
ahhxfeng <ahhxfeng@gmail.com>
2019-04-19 23:28:27 +0800

    'update'

--- flask_test/hello.py
+++ flask_test/hello.py
@@ -4,22 +4,26 @@
 __author__ = 'ahhxfeng'
 __version__ = '1.0.0'
 
-from flask import Flask
-from flask.ext.script import Manager
+from flask import Flask, render_template
+from flask_script import Manager
+
 app = Flask(__name__)
 Manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello world !!!</h1>'
+    # return '<h1>Hello world !!!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s</h1>' % name
+    # return '<h1>Hello %s</h1>' % name
+    return render_template('user.html', name=name)
 
 @app.route('/bad')
 def badtest():
-    return '<h1>bad test</h1>', 400
+    # return '<h1>bad test</h1>', 400
+    return render_template('bad.html'), 400
 
 if __name__ == "__main__":
     # app.run(debug=True)
