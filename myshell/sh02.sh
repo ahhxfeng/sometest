8d9fed51bdb6714f7fd3b1a0cd8c34b2f2e55af7
ahhxfeng <ahhxfeng@gmail.com>
2020-08-06 22:35:37 +0800

    update some shell for test

---
+++ myshell/sh02.sh
@@ -0,0 +1,9 @@
+#! bin/bash
+# version = 1.0
+# author = ahhxfeng
+#Program:
+#       show the name that user input
+
+read -p "Please input your name: " name
+read -p "Please input your lastname" lastname
+echo -e "\n Your full name is : $name $lastname"
